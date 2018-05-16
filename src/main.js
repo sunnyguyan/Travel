f91e24530d01e7394b415834bf84c1917963cc61
guyan <1821175203@qq.com.com>
2018-05-16 15:41:07 +0800

    package init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick' //移动端click300毫秒执行
+
+import './assets/styles/reset.css'
+//1像素边框问题
+import './assets/styles/border.css'
+
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
