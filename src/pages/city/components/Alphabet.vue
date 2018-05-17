1925cadbe6add394af1e21545bf54cda980af4a7
guyan <1821175203@qq.com.com>
2018-05-17 20:22:08 +0800

    perfomance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,11 @@
 <template>
   <ul class="list">
-      <li class="item" v-for="(item,key) in cities" :key="key">{{key}}</li>
+      <li class="item" v-for="(item,key) in letters" 
+      :key="item" @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      :ref="item">{{item}}</li>
   </ul>
 </template>
 
@@ -10,10 +15,52 @@ export default {
     props:{
         cities:Object
     },
-  data () {
-    return {
-    };
-  },
+    computed:{
+        letters(){
+            const letters=[]
+            for(let i in this.cities){
+                letters.push(i)
+            }
+            return letters
+        }
+    },
+    data () {
+        return {
+            touchstatus:false,
+            startY:0,
+            timer:null
+        };
+    },
+    updated(){
+       this.startY=this.$refs['A'][0].offsetTop
+    },
+    methods:{
+        handleLetterClick (e) {
+            console.log("kkk+++++"+e.target.innerText)
+            this.$emit('change',e.target.innerText)
+        },
+        handleTouchStart (){
+            this.touchStatus=true
+        },
+        handleTouchMove(e){
+            if(this.touchStatus){
+                if(this.timer){
+                    clearTimeout(this.timer)
+                }
+                this.timer=setTimeout(()=>{
+                    const touchY=e.touches[0].clientY-79
+                    const index=Math.floor((touchY-this.startY)/20)
+                    console.log("index+++"+index)
+                    if(index>=0&&index<this.letters.length){
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)
+            }
+        },
+        handleTouchEnd(){
+            this.touchStatus=false
+        }
+    }
 }
 
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,7 @@
             </div>
         </div>
       </div> 
-      <div class="area" v-for="(item,key) in cities" :key="key">
+      <div class="area" v-for="(item,key) in cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-topbottom" v-for="innerItem in item" :key="innerItem.id">{{innerItem.name}}</div>
@@ -33,7 +33,8 @@ export default {
   name:'CityList',
   props:{
     hot:Array,
-    cities:Object
+    cities:Object,
+    letter:String
   },
   data () {
     return {
@@ -41,6 +42,16 @@ export default {
   },
   mounted () {
     this.scroll =new BScroll(this.$refs.wrapper)
+  },
+  watch:{
+    letter(){
+     
+      if(this.letter){
+        const element=this.$refs[this.letter][0]
+        console.log("A+++"+element)
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 
